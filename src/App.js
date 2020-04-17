e73701f1b5635b72de78c83f0e7b1723622e5ec0
下沢廉 <shimosawaren@shimosawarennoMacBook-Pro.local>
2020-04-17 14:31:39 +0900

    counter_training

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { useState } from 'react'
 
 const App= () => {
-	const output = useState(1000)
-	console.log({output})
+	const [count,setCount] = useState(0)
+	const increment = () => setCount(count+1)
+	const decrement = () => setCount(count-1)
 
-	  return (
-    		<div>
-			This is a template for React app.
-   		 </div>
+	const increment2 = () => setCount(previousCount => previousCount + 1)
+	const decrement2 = () => setCount(previousCount => previousCount - 1)
+
+	const reset =() => setCount(0)
+
+	const twice =() => setCount(count * 2)
+
+	const devide3 =() => setCount (previousCount => {
+		if (previousCount % 3 === 0){
+			return previousCount / 3
+		}else{
+			return previousCount
+		}
+	})
+
+	return (
+	<>
+	<div>count:{count}</div>
+	<div>
+		<button onClick={increment}>+1</button>
+		<button onClick={decrement}>-1</button>
+	</div>
+	<div>
+		<button onClick={increment2}>+1</button>
+		<button onClick={decrement2}>-1</button>
+	</div>
+	<div>
+		<button onClick={reset}>Reset</button>
+		<button onClick={twice}>x2</button>
+		<button onClick={devide3}>３の倍数のときだけ３で割る</button>
+	</div>
+	
+	</>
   )
 }
 
