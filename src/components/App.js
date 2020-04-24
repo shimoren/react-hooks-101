4db0dd859c0db15577a246b8daa2274d892c0fd0
下沢廉 <shimosawaren@shimosawarennoMacBook-Pro.local>
2020-04-24 21:49:15 +0900

    display-events, refactoe & implement remove event button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { useReducer, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Event from './Event'
 import reducer from '../reducers'
+
 
 const App= () => {
 
@@ -53,7 +55,7 @@ const App= () => {
           </tr>
         </thead>
         <tbody>
-
+          {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
         </tbody>
 
       </table>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -49,7 +49,7 @@ const events = (state = [], action) => {
             }
             return [...state, { id: id, ...event}]
         case 'DELETE_EVENT':
-            return state
+            return state.filter(event => event.id !== action.id)
         case 'DELETE_ALL_EVENTS':
             return []
         default:
