415f9064e6ddef12986c717217fe38671630059b
下沢廉 <shimosawaren@shimosawarennoMacBook-Pro.local>
2020-04-27 10:20:38 +0900

    setup react context provider

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,23 @@ import React, { useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import EventForm from './EventForm'
 import Events from './Events'
+import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
+
+console.log({AppContext})
 
 
 const App= () => {
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
+    <AppContext.Provider value={'Hello, i am me'}>
     <div className="container">
     <EventForm state={state} dispatch={dispatch} />
     <Events state={state} dispatch={dispatch} />
       
     </div>
+    </AppContext.Provider>
   )
 }
 
--- src/components/Events.js
+++ src/components/Events.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Event from './Event'
+import AppContext from '../contexts/AppContext'
+import App from './App'
 
 const Events = ({state, dispatch}) => {
+    const value = useContext(AppContext)
+
     return(
         <>
+        <div>{value}</div>
         <h4>イベント一覧</h4>
         <table className="table table-hover">
         <thead>
